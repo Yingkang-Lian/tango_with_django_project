461bb16bcb8fec3eab68c74d6e247d59c2e7b411
Yingkang Lian <2744904l@student.gla.ac.uk>
2025-02-14 02:37:59 +0000

    Completed chapter 8

--- rango/views.py
+++ rango/views.py
@@ -51,6 +51,7 @@ def add_category(request):
             print(form.errors)
     
     return render(request, 'rango/add_category.html', {'form': form})
+
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
